87b73878008b91a07bf643113a5e65967db2485d
AtomixDem <74184968+AtomixDem@users.noreply.github.com>
2024-02-04 14:27:00 +0100

    Create CreateDB.php

---
+++ Query/CreateDB.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "127.0.0.1";
+$user = "root";
+$password = "";
+$database = "test";
+
+$connessione = new mysqli($host, $user, $password, $database); // Connessione al DB
+
+
+$sql = "CREATE DATABASE db_prova"; // Comando che deve eseguire MySQL per creare un nuovo database
+
+if($connessione->query($sql) == true) { // Verifichiamo che la creazione del DB è avvenuta con successo:
+    echo "Database creato con successo.";
+} else {
+    echo "Errore creazione database: " . $connessione->error; // Stampa gli eventuali errori
+}
+
+?>
